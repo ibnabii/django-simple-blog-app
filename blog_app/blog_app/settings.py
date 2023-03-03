2143263302a01d60c7cb8863ba514f843b90d173
Artur Pasieczny <bla@bla.pl>
2023-03-03 17:00:32 +0100

    - Added navbar translations (at least part to test autotranslation) to PL and DE - Added cookie/header/fallback to global settings language selection Todo: - Internationalization: in URL patterns

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'blog_app.urls'
@@ -125,3 +127,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOCALE_PATHS = (BASE_DIR / 'locale/', )
